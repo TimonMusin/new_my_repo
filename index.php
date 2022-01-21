79be75a64e5952d9644a49bc2ec2f34e1b5d2c7d
noNAMER <empty>
2022-01-21 18:24:51 +0400

    index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+
+</head>
+<body>
+<header>
+    <div class="container .bg-light">
+
+        <div class="header_1">
+
+        </div>
+        <div class="header_2">
+            
+        </div>
+    </div>
+</header>
+</body>
+</html>
